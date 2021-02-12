152c6adadc7db2500d163b1e25418040144ed284
Surjeet Mohanty <surjeetmohanty26@gmail.com>
2021-02-12 16:35:37 +0530

    add

--- src/main/java/com/example/sonar/SpringbootSonarqubeTestcaseApplication.java
+++ src/main/java/com/example/sonar/SpringbootSonarqubeTestcaseApplication.java
@@ -3,7 +3,6 @@ package com.example.sonar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.sonar.controller"})
 public class SpringbootSonarqubeTestcaseApplication {
